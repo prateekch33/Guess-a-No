40ed855cc43b6e5f33214c5b9dd0a9fb6f9cdc6b
prateekch33 <prateekchandra27@outlook.com>
2021-02-25 00:11:18 +0530

    init

---
+++ gan.js
@@ -0,0 +1,14 @@
+var num=prompt("Guess a No.")
+var snum=7
+if(num<snum)
+{
+    alert("The No. is too less!, Try Again!");
+}
+else if(num>snum) 
+{
+    alert("The No. is too big!, Try Again!");
+}
+else
+{
+    alert("You Guessed it Right!")
+}
